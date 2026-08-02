e41bf4385451ba3e9b5eb5cf9e4a77350dc1d15d
Gustavo Souza <gustavosouzap28@gmail.com>
2026-08-02 19:04:51 -0300

    feat: Coloca o Lomobk na classe para abstracao

--- src/main/java/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package java10x.CadastroDeNinjas.Missoes;
 
 import jakarta.persistence.*;
 import java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id

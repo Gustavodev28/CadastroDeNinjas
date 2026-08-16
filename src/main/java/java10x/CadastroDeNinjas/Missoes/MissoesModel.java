a9afcc32287bad8328b6a1fc0b96ea649caf9239
Gustavo Souza <gustavosouzap28@gmail.com>
2026-08-16 13:45:31 -0300

    fix: Correcao de sintaxe em @Column(name = Dificuldade)

--- src/main/java/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -17,13 +17,13 @@ import java.util.List;
 public class MissoesModel {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     @Column(name = "ID")
-    private long id;
-@Column(name = "Nome_da_missao")
+    private Long id;
+    @Column(name = "Nome_da_missao")
     private String nomeDaMissao;
-@Column(name = "Dificiuldade")
+    @Column(name = "Dificuldade")
     private String dificuldade;
 
     //Uma missao pode ter varios ninjas
